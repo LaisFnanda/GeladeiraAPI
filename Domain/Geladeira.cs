173f57b53e4b73a0609b2298fd3f993f52653051
LaisFnanda <lais.589@gmail.com>
2024-08-24 22:51:44 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Geladeira.cs
@@ -0,0 +1,108 @@
+﻿namespace Domain
+{
+    public sealed class Geladeira
+    {
+        private Stack<Andar> _Andares;
+
+        public int? ID { get; set; }
+
+        // private const int numAndares = 3;
+
+        public Geladeira()
+        {
+                        
+            _Andares = new Stack<Andar>();
+
+            _Andares.Push(new Andar(0, "Charcutaria, Carnes e Ovos"));
+            _Andares.Push(new Andar(1, "Laticínios e Enlatados"));
+            _Andares.Push(new Andar(2, "Frutas e Verduras"));
+        }
+
+        private List<Andar> RetornarAndares(int numAndar)
+        {
+            var lstAndares = _Andares.ToList();
+
+            Validarandares(numAndar, lstAndares);
+
+            return lstAndares;
+        }
+
+        private static void Validarandares(int numAndar, List<Andar> lstAndares)
+        {
+            if (numAndar < 0 || numAndar >= lstAndares.Count)
+                throw new Exception("Numero do andar inválido!");
+        }
+
+        private static Container? RetornarContainer(int numAndar, int numContainer, List<Andar> lstAndares)
+        {
+            Validarandares(numAndar, lstAndares);
+
+            var container = lstAndares[numAndar].RetornarContainer(numContainer);
+            return container;
+        }
+
+        public void AdicionarItem(int numAndar, int numContainer, int posicao, Item item)
+        {
+            var arrAndares = RetornarAndares(numAndar);
+
+            Container? container = RetornarContainer(numAndar, numContainer, arrAndares);
+
+            if (container == null)
+                throw new Exception("Numero do container inválido!");
+
+            container.AdicionarItem(posicao, item);
+        }
+
+
+        public void RemoverItem(int numAndar, int numContainer, int posicao)
+        {
+            var arrAndares = RetornarAndares(numAndar);
+
+            Container? container = RetornarContainer(numAndar, numContainer, arrAndares);
+
+            container?.RetornarItem(posicao);
+        }
+
+        public void LimparContainer(int numAndar, int numContainer)
+        {
+            var arrAndares = RetornarAndares(numAndar);
+
+            Container? container = RetornarContainer(numAndar, numContainer, arrAndares);
+
+            if (!Convert.ToBoolean(container?.EstaVazio()))
+            {
+                container?.LimparContainer();
+            }
+            else
+            {
+                Console.WriteLine("Container está vazio!");
+                return;
+            }
+        }
+
+        public void AddItensAoContainer(int numAndar, int numContainer, List<Item> itens)
+        {
+            var arrAndares = RetornarAndares(numAndar);
+
+            Container? container = RetornarContainer(numAndar, numContainer, arrAndares);
+
+            if (!Convert.ToBoolean(container?.EstaCheio()))
+                container?.AdicionarItens(itens);
+            else
+            {
+                Console.WriteLine("Container já está cheio!");
+                return;
+            }
+
+        }
+
+        public void ImprimeConteudo()
+        {
+            var lstAndares = _Andares.ToList();
+
+            foreach (var andar in lstAndares)
+                andar.ImprimirItens();
+        }
+
+    }
+}
---
+++ GeladeiraAPI/Controllers/GeladeiraController.cs
@@ -0,0 +1,144 @@
+using Domain;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace GeladeiraAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GeladeiraController : ControllerBase
+    {
+
+         private List<Item> lstgeladeira;
+
+        public GeladeiraController()
+        {
+            // andar 2 - Hortifrutis
+            var objItemMaca = new Item() { Descricao = "Maçã", Quantidade = 1, UnidadeQtd = "Unidade", Classificacao = "Fruta", ID = 1 };
+            var objItemBanana = new Item() { Descricao = "Banana", Quantidade = 1, UnidadeQtd = "Cacho", Classificacao = "Fruta", ID = 2 };
+            var objItemLaranja = new Item() { Descricao = "Laranja", Quantidade = 1, UnidadeQtd = "Duzia", Classificacao = "Fruta", ID = 3 };
+
+            // andar 1 - Laticínios e Enlatados
+            var objItemLeite = new Item() { Descricao = "Leite", Quantidade = 1, UnidadeQtd = "Litro", Classificacao = "Laticínio", ID = 4 };
+            var objItemQueijo = new Item() { Descricao = "Queijo", Quantidade = 1, UnidadeQtd = "Unidade", Classificacao = "Laticínio", ID = 5 };
+            var objItemMilho = new Item() { Descricao = "Milho Enlatado", Quantidade = 1, UnidadeQtd = "Lata", Classificacao = "Enlatado", ID = 6 };
+              
+            // andar 0 - Charcutaria, Carnes e Ovos
+            var objItemPresunto = new Item() { Descricao = "Presunto", Quantidade = 100, UnidadeQtd = "Gramas", Classificacao = "Charcutaria", ID = 7 };
+            var objItemOvos = new Item() { Descricao = "Ovos", Quantidade = 1, UnidadeQtd = "Duzia", Classificacao = "Ovo", ID = 8 };
+            var objItemCarne = new Item() { Descricao = "Carne", Quantidade = 1, UnidadeQtd = "Kilo", Classificacao = "Carne", ID = 9 };
+
+            lstgeladeira = new List<Item>() 
+            { 
+                objItemBanana, objItemBanana, objItemLaranja, objItemLeite, objItemQueijo, objItemMilho, 
+                objItemPresunto, objItemOvos, objItemCarne, objItemCarne
+            };
+        }
+
+
+        [HttpGet]
+        public IEnumerable<Item> Get() => lstgeladeira;
+
+
+        [HttpHead]
+        public List<Item> Head() => lstgeladeira;
+
+        
+
+        [HttpGet("{id}")]
+        public Item Get(int id)
+        {
+            foreach (var item in lstgeladeira)
+            {
+                if (item.ID == id)
+                    return item;
+            }
+            return new Item();
+        }
+
+        [HttpOptions]
+        public Item Option(int id)
+        {
+            foreach (var item in lstgeladeira)
+            {
+                if (item.ID == id)
+                    return item;
+            }
+            return new Item();
+        }
+
+        [HttpPatch("Alterar Descrição Item")]
+        public List<Item> Patch(int id, [FromBody] string descricao, int qtde, string unidade, string classificacao)
+        {
+            foreach (var item in lstgeladeira)
+            {
+                if (item.ID == id)
+                {
+                    //DisplayName("Teste");                 
+                    item.Descricao = descricao;
+                    item.UnidadeQtd = unidade;
+                    item.Quantidade = qtde;
+                    item.Classificacao = classificacao;
+                }
+
+            }
+            return lstgeladeira;
+        }
+
+        [HttpPost]
+        public List<Item> Post([FromBody] Item item)
+        {
+            lstgeladeira.Add(item);
+
+            return lstgeladeira;
+        }
+
+        [HttpPut("Alterar Descrição Item")]
+        public List<Item> Put(int id, string descricao)
+        {
+            foreach (var item in lstgeladeira)
+            {
+                if (item.ID == id)
+                {                                   
+                    item.Descricao = descricao;
+                }
+                    
+            }
+
+            return lstgeladeira;
+        }
+
+        [HttpDelete("Excluir um item")]
+        public List<Item> Delete(int id)
+        {
+            try
+            {
+                foreach (var item in lstgeladeira)
+                {
+                    if (item.ID == id)
+                    {
+                        lstgeladeira.Remove(item);
+                        break;
+                    }
+
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return lstgeladeira;
+        }         
+
+    }
+
+}
+
+    
+
